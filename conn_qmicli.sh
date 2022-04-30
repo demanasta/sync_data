fb1a33eec7ecbae50a9d43c7e732e575306394da
mitsos.krini <dganastatiou@gmail.com>
2022-04-30 12:10:58 +0100

    add mknod to reconnect ttyUSB

--- conn_qmicli.sh
+++ conn_qmicli.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 VERSION="conn_qmicli - v0.1b12"
 
-# ////////////////////////////////////////////////////////////////////////////////
-# HELP FUNCTION
+## ////////////////////////////////////////////////////////////////////////////////
+## HELP FUNCTION
 
 function help {
   echo " Program Name : conn_qmicli.sh"
@@ -17,40 +17,42 @@ function help {
   echo " History:"
   echo "   2021.09.21 : Initial beta version"
   echo "   2022.03.20 : Add config file, documentation, clen code"
+  echo "   2022.04.29 : Ad mknod to enable ttyUSB* if disconected"
  }
 
-#Set bash parameters
+## Set bash parameters
 set -o pipefail
 
-# TODO check if config file exist
+## TODO check if config file exist
 source $1
 
 LOG=.log/conn_$(date -u +%Y%m%d).log
 
 
-# Check if necessary programms exista qmicli
+## Check if necessary programms exista qmicli
 if ! [ -x "$(command -v qmicli)" ]
 then
      echo "$(date +%Y.%m.%d_%H:%M:%S) [ERROR]: libqmi-utils is not install " >> ${LOG}   
      exit 1
 
 fi
-# CHeck udhcpc
+
+## CHeck udhcpc
 if ! [ -x "$(command -v udhcpc)" ]
 then
      echo "$(date +%Y.%m.%d_%H:%M:%S) [ERROR]: udhcpc is not install " >> ${LOG}   
      exit 1
 fi
 
-# ////////////////////////////////////////////////////////////////////////////////
-# GET CML Arguments
+## ////////////////////////////////////////////////////////////////////////////////
+## GET CML Arguments
 if [ "$#" == 0 ]
 then
     echo "[ERROR]: No input file"
     help
 fi
 
-# TODO get arguments chec if config file exists
+## TODO get arguments chec if config file exists
 #while [ $# -gt 0 ]
 #do
 #    case "$1" in
@@ -59,10 +61,10 @@ fi
 
 #wait
 
-# down if wlan0 is up
+## TODO down if wlan0 is up check if exist
 sudo ifconfig wlan0 down >> ${LOG} 2>&1
 
-#check connection to paliki
+## check connection to paliki
 nc -z "${CHECKIP}" "${CHECKPORT}" >/dev/null 2>&1
 checknet=$?
 
@@ -75,7 +77,7 @@ else
 
     echo "$(date +%Y.%m.%d_%H:%M:%S) [WRNG]: Raspberry is OFFLINE. Try new connection" >>${LOG}
 
-    # set encounter to 0
+    ## set encounter to 0
     enc=0
     while [ ${enc} -lt 60 ]
     do
@@ -112,10 +114,10 @@ else
 
 #sudo ifconfig wlan0 down >> ${LOG} 2>&1
 
-	    # set static ip
+	    ## set static ip
 	    sudo udhcpc -i wwan0 >> ${LOG}
 	sleep 10
-	    # set wwan0 as default
+	    ## set wwan0 as default
 	    sudo route add default dev wwan0 >> ${LOG}
 	    
 	    nc -z "${CHECKIP}" "${CHECKPORT}" >/dev/null 2>&1
@@ -131,13 +133,23 @@ else
 	    fi
 
         else
-	    #check ttyUSB
+	    ## check ttyUSB
 	    echo "$(date +%Y.%m.%d_%H:%M:%S) [WRNG]: /dev/ttyUSB* does not exist ($enc)" >> ${LOG}
 	    sleep 5
 	    let enc++
         fi
 
     done
+    ## If ttyUSB not exist enable using kmnod and then reboot raspberry
+    echo "$(date +%Y.%m.%d_%H:%M:%S) [DEBUG]: enable ttyUSB* runing mknod" >> ${LOG} 
+    mknod /dev/ttyUSB0 c 188 0
+    ## Check if ttyUSB exist
+    if [ -c /dev/ttyUSB0 ] && [ -c /dev/ttyUSB1 ] && [ -c /dev/ttyUSB2 ] && [ -c /dev/ttyUSB3 ] && [ -c /dev/ttyUSB4 ]   
+    then
+	echo "$(date +%Y.%m.%d_%H:%M:%S) [WRNG]: /dev/ttyUSB* exist " >> ${LOG}
+    else 
+	mknod /dev/ttyUSB0 c 188 0
+    fi
     echo "$(date +%Y.%m.%d_%H:%M:%S) [ERROR]: Ras will reboot. no connection" >> ${LOG}
     sudo reboot
 
