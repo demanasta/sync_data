debc9bf6bfe624bae7b32be7861f1f146b7b8281
mitsos.proion <dganastasiou@gmail.com>
2022-12-22 15:30:02 +0200

    work on #6

--- conn_qmicli.sh
+++ conn_qmicli.sh
@@ -32,7 +32,7 @@ LOG=.log/conn_$(date -u +%Y%m%d).log
 ## Check if necessary programms exista qmicli
 if ! [ -x "$(command -v qmicli)" ]
 then
-     echo "$(date +%Y.%m.%d_%H:%M:%S) [ERROR]: libqmi-utils is not install " >> ${LOG}   
+     echo "$(date +%Y.%m.%d_%H:%M:%S) [ERROR]: libqmi-utils is not install " >> ${LOG}
      exit 1
 
 fi
@@ -40,7 +40,7 @@ fi
 ## CHeck udhcpc
 if ! [ -x "$(command -v udhcpc)" ]
 then
-     echo "$(date +%Y.%m.%d_%H:%M:%S) [ERROR]: udhcpc is not install " >> ${LOG}   
+     echo "$(date +%Y.%m.%d_%H:%M:%S) [ERROR]: udhcpc is not install " >> ${LOG}
      exit 1
 fi
 
@@ -83,74 +83,106 @@ else
     do
 
         if [ -c /dev/ttyUSB0 ] && [ -c /dev/ttyUSB1 ] && [ -c /dev/ttyUSB2 ] && [ -c /dev/ttyUSB3 ] && [ -c /dev/ttyUSB4 ]
-	then
+    	then
+            ## If ttyUSB exist setup the connection
+    	    echo "$(date +%Y.%m.%d_%H:%M:%S) [DEBUG]: ttyUSB exist, start qmicli" >> ${LOG}
+            #sleep 60
+    	    sudo socat - /dev/ttyUSB2,crnl <<<'AT+CPIN='${CPIN}  >> ${LOG}
 
-	    echo "$(date +%Y.%m.%d_%H:%M:%S) [DEBUG]: ttyUSB exist, start qmicli" >> ${LOG}
-    #sleep 60
-    	    sudo socat - /dev/ttyUSB2 <<<'AT+CPIN='${CPIN}  >> ${LOG}
+    	    qmicli -d /dev/cdc-wdm0 --dms-set-operating-mode='online' >> ${LOG}
 
-	    qmicli -d /dev/cdc-wdm0 --dms-set-operating-mode='online' >> ${LOG}
+    	    qmicli -d /dev/cdc-wdm0 --dms-get-operating-mode >> ${LOG}
 
-	    qmicli -d /dev/cdc-wdm0 --dms-get-operating-mode >> ${LOG}
+    	    qmicli -d /dev/cdc-wdm0 --nas-get-signal-strength >> ${LOG}
 
-	    qmicli -d /dev/cdc-wdm0 --nas-get-signal-strength >> ${LOG}
+    	    qmicli -d /dev/cdc-wdm0 --nas-get-home-network >> ${LOG}
 
-	    qmicli -d /dev/cdc-wdm0 --nas-get-home-network >> ${LOG}
+	        ifconfig >> ${LOG}
 
-	    ifconfig >> ${LOG}
+	        ip link set wwan0 down >> ${LOG}
 
-	    ip link set wwan0 down >> ${LOG}
+    	    echo 'Y' > /sys/class/net/wwan0/qmi/raw_ip
 
-	    echo 'Y' > /sys/class/net/wwan0/qmi/raw_ip
+    	    ip link set wwan0 up >> ${LOG}
 
-	    ip link set wwan0 up >> ${LOG}
+    	    ifconfig >> ${LOG}
 
-	    ifconfig >> ${LOG}
+    	    sudo qmicli --device=/dev/cdc-wdm0 \
+        		    --device-open-net="net-raw-ip|net-no-qos-header"\
+		            --wds-start-network="ip-type=4,apn=${APNNAME}, username=${USERNAME}, password=${USERPASSWD}" \
+        		    --client-no-release-cid >> ${LOG}
 
-	    sudo qmicli --device=/dev/cdc-wdm0 \
-    		    --device-open-net="net-raw-ip|net-no-qos-header"\
-		    --wds-start-network="ip-type=4,apn=${APNNAME}, username=${USERNAME}, password=${USERPASSWD}" \
-		    --client-no-release-cid >> ${LOG}
+            #sudo ifconfig wlan0 down >> ${LOG} 2>&1
 
-#sudo ifconfig wlan0 down >> ${LOG} 2>&1
+	        ## set static ip
+    	    sudo udhcpc -i wwan0 >> ${LOG}
+        	sleep 10
+	        ## set wwan0 as default
+    	    sudo route add default dev wwan0 >> ${LOG}
 
-	    ## set static ip
-	    sudo udhcpc -i wwan0 >> ${LOG}
-	sleep 10
-	    ## set wwan0 as default
-	    sudo route add default dev wwan0 >> ${LOG}
-	    
-	    nc -z "${CHECKIP}" "${CHECKPORT}" >/dev/null 2>&1
-	    checknet=$?
+    	    nc -z "${CHECKIP}" "${CHECKPORT}" >/dev/null 2>&1
+    	    checknet=$?
 
-	    if [ ${checknet} -eq 0 ]
-	    then
-	        echo "$(date +%Y.%m.%d_%H:%M:%S) [DEBUG]: Raspberry is online!" >> ${LOG}
-	        exit 0
-	    else
-	        echo "$(date +%Y.%m.%d_%H:%M:%S) [WRNG]: Raspberry is OFFLINE. Try new connection" >>${LOG}
-	        enc=0
-	    fi
+    	    if [ ${checknet} -eq 0 ]
+    	    then
+	            echo "$(date +%Y.%m.%d_%H:%M:%S) [DEBUG]: Raspberry is online!" >> ${LOG}
+
+                ## If rasberry is online exit the script with status 0
+	            exit 0
+    	    else
+	            echo "$(date +%Y.%m.%d_%H:%M:%S) [WRNG]: Raspberry is OFFLINE. Try new connection" >>${LOG}
+	            enc=0
+    	    fi
 
         else
-	    ## check ttyUSB
-	    echo "$(date +%Y.%m.%d_%H:%M:%S) [WRNG]: /dev/ttyUSB* does not exist ($enc)" >> ${LOG}
-	    sleep 5
-	    let enc++
-        fi
+            ## If ttyUSB does not exist....
+    	    ## check ttyUSB
+	        echo "$(date +%Y.%m.%d_%H:%M:%S) [WRNG]: /dev/ttyUSB* does not exist ($enc)" >> ${LOG}
 
+            ## Check lsusb if router esist
+            echo "$(date +%Y.%m.%d_%H:%M:%S) [DEBUG]: Check lsusb output" >> ${LOG}
+            lsusb >>${LOG}
+
+            ## Sleep 5sec add encounter and restart while
+    	    sleep 5
+    	    let enc++
+        fi
     done
+
     ## If ttyUSB not exist enable using kmnod and then reboot raspberry
-    echo "$(date +%Y.%m.%d_%H:%M:%S) [DEBUG]: enable ttyUSB* runing mknod" >> ${LOG} 
+    echo "$(date +%Y.%m.%d_%H:%M:%S) [DEBUG]: enable ttyUSB* runing mknod" >> ${LOG}
     mknod /dev/ttyUSB0 c 188 0
     ## Check if ttyUSB exist
-    if [ -c /dev/ttyUSB0 ] && [ -c /dev/ttyUSB1 ] && [ -c /dev/ttyUSB2 ] && [ -c /dev/ttyUSB3 ] && [ -c /dev/ttyUSB4 ]   
+
+    if [ -c /dev/ttyUSB0 ] && [ -c /dev/ttyUSB1 ] && [ -c /dev/ttyUSB2 ] && [ -c /dev/ttyUSB3 ] && [ -c /dev/ttyUSB4 ]
     then
-	echo "$(date +%Y.%m.%d_%H:%M:%S) [WRNG]: /dev/ttyUSB* exist " >> ${LOG}
-    else 
-	mknod /dev/ttyUSB0 c 188 0
+    	echo "$(date +%Y.%m.%d_%H:%M:%S) [WRNG]: /dev/ttyUSB* exists " >> ${LOG}
+        exit 0
+    else
+#	    mknod /dev/ttyUSB0 c 188 0
+
+        ## TODO Add new solution to stop all processing before restart the raspberry
+        ## list all processing
+        # fuser /dev/ttyUSB0
+
+        ## if fuser return processing killthem by pid
+        # sudo pkill <fuser returns>
+
+        ## if fuser return nothing then try lsof
+        # sudo lsof /def/ttyUSB0
+
+        ## kill all returns from lsof
+        # sudo pkill <lsof retuns>
+
+        ## send lsusb debug to log file
+        echo "$(date +%Y.%m.%d_%H:%M:%S) [DEBUG]: Check lsusb output" >> ${LOG}
+        lsusb >> ${LOG}
+
+        ## REBOOT the raspberry
+        echo "$(date +%Y.%m.%d_%H:%M:%S) [ERROR]: Ras will reboot. no connection" >> ${LOG}
+        sudo reboot
     fi
-    echo "$(date +%Y.%m.%d_%H:%M:%S) [ERROR]: Ras will reboot. no connection" >> ${LOG}
-    sudo reboot
+#    echo "$(date +%Y.%m.%d_%H:%M:%S) [ERROR]: Ras will reboot. no connection" >> ${LOG}
+#    sudo reboot
 
 fi
